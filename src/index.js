2074a0ff5a0b6cef3e47c9677bc971c28cd85e56
Edison Matos <edison.matos@live.com>
2023-11-20 21:44:57 -0300

    feat(app): Install dotenv and prismaORM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-const app = require("../src/server");
+const app = require("../src/app");
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Server runing on port ${port}`);
+  console.log(`Server runing on port ${port}. Check http://localhost:3000`);
 });
